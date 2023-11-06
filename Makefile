wormhole-foundation/wormhole

Makefile: set CGO_ENABLED=1
This is required for building the guardiand binary

Makefile
@@ -35,6 +35,7 @@ generate: dirs
 node: $(BIN)/guardiand
 
 .PHONY: $(BIN)/guardiand
+$(BIN)/guardiand: CGO_ENABLED=1
 $(BIN)/guardiand: dirs generate
 	@# The go-ethereum and celo-blockchain packages both implement secp256k1 using the exact same header, but that causes duplicate symbols.
 	cd node && go build -ldflags "-X github.com/certusone/wormhole/node/pkg/version.version=${VERSION} -extldflags -Wl,--allow-multiple-definition" \

deltaswapio/deltaswap

(cherry picked from commit fc39351b7188fb9b98a2a5bfeddcfd13a933a82c)

Makefile
@@ -35,6 +35,7 @@ generate: dirs
 node: $(BIN)/phylaxd
 
 .PHONY: $(BIN)/phylaxd
+$(BIN)/phylaxd: CGO_ENABLED=1
 $(BIN)/phylaxd: dirs generate
 	@# The go-ethereum and celo-blockchain packages both implement secp256k1 using the exact same header, but that causes duplicate symbols.
 	cd node && go build -ldflags "-X github.com/deltaswapio/deltaswap/node/pkg/version.version=${VERSION} -extldflags -Wl,--allow-multiple-definition" \