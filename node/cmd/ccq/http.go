friend413/Hole

CCQ: Query Server (#3422)
* CCQ: Query Server

* More rework

* Clean up p2p code

* Health check change

* node: support http PUT or POST for new ccq queries

---------

Co-authored-by: Jeff Schroeder <jeffschroeder@computer.org>

node/cmd/ccq/http.go
@@ -1,0 +1,179 @@
+package ccq
+
+import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sort"
+	"time"
+
+	"github.com/certusone/wormhole/node/pkg/common"
+	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+	"github.com/certusone/wormhole/node/pkg/query"
+	"github.com/gorilla/mux"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+)
+
+type queryRequest struct {
+	Bytes     string `json:"bytes"`
+	Signature string `json:"signature"`
+}
+
+type queryResponse struct {
+	Bytes      string   `json:"bytes"`
+	Signatures []string `json:"signatures"`
+}
+
+type httpServer struct {
+	topic            *pubsub.Topic
+	logger           *zap.Logger
+	env              common.Environment
+	permissions      Permissions
+	signerKey        *ecdsa.PrivateKey
+	pendingResponses *PendingResponses
+}
+
+func (s *httpServer) handleQuery(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for all requests.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+
+		w.Header().Set("Access-Control-Allow-Methods", "PUT, POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Api-Key")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	var q queryRequest
+	err := json.NewDecoder(r.Body).Decode(&q)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// There should be one and only one API key in the header.
+	apiKey, exists := r.Header["X-Api-Key"]
+	if !exists || len(apiKey) != 1 {
+		s.logger.Debug("received a request without an api key", zap.Stringer("url", r.URL), zap.Error(err))
+		http.Error(w, "api key is missing", http.StatusUnauthorized)
+		return
+	}
+
+	queryRequestBytes, err := hex.DecodeString(q.Bytes)
+	if err != nil {
+		s.logger.Debug("failed to decode request bytes", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	signature, err := hex.DecodeString(q.Signature)
+	if err != nil {
+		s.logger.Debug("failed to decode signature bytes", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    signature,
+	}
+
+	if status, err := validateRequest(s.logger, s.env, s.permissions, s.signerKey, apiKey[0], signedQueryRequest); err != nil {
+		// Don't need to log here because the details were logged in the function.
+		http.Error(w, err.Error(), status)
+		return
+	}
+
+	m := gossipv1.GossipMessage{
+		Message: &gossipv1.GossipMessage_SignedQueryRequest{
+			SignedQueryRequest: signedQueryRequest,
+		},
+	}
+
+	b, err := proto.Marshal(&m)
+	if err != nil {
+		s.logger.Error("failed to marshal gossip message", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	pendingResponse := NewPendingResponse(signedQueryRequest)
+	added := s.pendingResponses.Add(pendingResponse)
+	if !added {
+		http.Error(w, "Duplicate request", http.StatusBadRequest)
+		return
+	}
+
+	err = s.topic.Publish(r.Context(), b)
+	if err != nil {
+		s.logger.Error("failed to publish gossip message", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		s.pendingResponses.Remove(pendingResponse)
+		return
+	}
+
+	// Wait for the response or timeout
+	select {
+	case <-time.After(query.RequestTimeout + 5*time.Second):
+		http.Error(w, "Timed out waiting for response", http.StatusGatewayTimeout)
+	case res := <-pendingResponse.ch:
+		resBytes, err := res.Response.Marshal()
+		if err != nil {
+			s.logger.Error("failed to marshal response", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			break
+		}
+		// Signature indices must be ascending for on-chain verification
+		sort.Slice(res.Signatures, func(i, j int) bool {
+			return res.Signatures[i].Index < res.Signatures[j].Index
+		})
+		signatures := make([]string, 0, len(res.Signatures))
+		for _, s := range res.Signatures {
+			// ECDSA signature + a byte for the index of the guardian in the guardian set
+			signature := fmt.Sprintf("%s%02x", s.Signature, uint8(s.Index))
+			signatures = append(signatures, signature)
+		}
+		w.Header().Add("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(&queryResponse{
+			Signatures: signatures,
+			Bytes:      hex.EncodeToString(resBytes),
+		})
+		if err != nil {
+			s.logger.Error("failed to encode response", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	s.pendingResponses.Remove(pendingResponse)
+}
+
+func (s *httpServer) handleHealth(w http.ResponseWriter, r *http.Request) {
+	s.logger.Debug("health check")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "ok")
+}
+
+func NewHTTPServer(addr string, t *pubsub.Topic, permissions Permissions, signerKey *ecdsa.PrivateKey, p *PendingResponses, logger *zap.Logger, env common.Environment) *http.Server {
+	s := &httpServer{
+		topic:            t,
+		permissions:      permissions,
+		signerKey:        signerKey,
+		pendingResponses: p,
+		logger:           logger,
+		env:              env,
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/query", s.handleQuery).Methods("PUT", "POST", "OPTIONS")
+	r.HandleFunc("/v1/health", s.handleHealth).Methods("GET")
+	return &http.Server{
+		Addr:              addr,
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+}

node/cmd/ccq/pending_request.go
@@ -1,0 +1,60 @@
+package ccq
+
+import (
+	"encoding/hex"
+	"sync"
+
+	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+)
+
+type PendingResponse struct {
+	req *gossipv1.SignedQueryRequest
+	ch  chan *SignedResponse
+}
+
+func NewPendingResponse(req *gossipv1.SignedQueryRequest) *PendingResponse {
+	return &PendingResponse{
+		req: req,
+		ch:  make(chan *SignedResponse),
+	}
+}
+
+type PendingResponses struct {
+	pendingResponses map[string]*PendingResponse
+	mu               sync.RWMutex
+}
+
+func NewPendingResponses() *PendingResponses {
+	return &PendingResponses{
+		pendingResponses: make(map[string]*PendingResponse),
+	}
+}
+
+func (p *PendingResponses) Add(r *PendingResponse) bool {
+	signature := hex.EncodeToString(r.req.Signature)
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if _, ok := p.pendingResponses[signature]; ok {
+		// the request w/ this signature is already being handled
+		// don't overwrite
+		return false
+	}
+	p.pendingResponses[signature] = r
+	return true
+}
+
+func (p *PendingResponses) Get(signature string) *PendingResponse {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	if r, ok := p.pendingResponses[signature]; ok {
+		return r
+	}
+	return nil
+}
+
+func (p *PendingResponses) Remove(r *PendingResponse) {
+	signature := hex.EncodeToString(r.req.Signature)
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	delete(p.pendingResponses, signature)
+}

node/cmd/ccq/query_server.go
@@ -1,0 +1,184 @@
+// Note: To generate a signer key file do: guardiand keygen --block-type "CCQ SERVER SIGNING KEY" /path/to/key/file
+// You will need to add this key to ccqAllowedRequesters in the guardian configs.
+
+package ccq
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/certusone/wormhole/node/pkg/common"
+	"github.com/certusone/wormhole/node/pkg/telemetry"
+	"github.com/certusone/wormhole/node/pkg/version"
+	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	ipfslog "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+const CCQ_SERVER_SIGNING_KEY = "CCQ SERVER SIGNING KEY"
+
+var (
+	envStr            *string
+	p2pNetworkID      *string
+	p2pPort           *uint
+	p2pBootstrap      *string
+	listenAddr        *string
+	nodeKeyPath       *string
+	signerKeyPath     *string
+	permFile          *string
+	ethRPC            *string
+	ethContract       *string
+	logLevel          *string
+	telemetryLokiURL  *string
+	telemetryNodeName *string
+)
+
+const DEV_NETWORK_ID = "/wormhole/dev"
+
+func init() {
+	envStr = QueryServerCmd.Flags().String("env", "", "environment (dev, test, prod)")
+	p2pNetworkID = QueryServerCmd.Flags().String("network", DEV_NETWORK_ID, "P2P network identifier")
+	p2pPort = QueryServerCmd.Flags().Uint("port", 8995, "P2P UDP listener port")
+	p2pBootstrap = QueryServerCmd.Flags().String("bootstrap", "", "P2P bootstrap peers (comma-separated)")
+	nodeKeyPath = QueryServerCmd.Flags().String("nodeKey", "", "Path to node key (will be generated if it doesn't exist)")
+	signerKeyPath = QueryServerCmd.Flags().String("signerKey", "", "Path to key used to sign unsigned queries")
+	listenAddr = QueryServerCmd.Flags().String("listenAddr", "[::]:6069", "Listen address for query server (disabled if blank)")
+	permFile = QueryServerCmd.Flags().String("permFile", "", "JSON file containing permissions configuration")
+	ethRPC = QueryServerCmd.Flags().String("ethRPC", "", "Ethereum RPC for fetching current guardian set")
+	ethContract = QueryServerCmd.Flags().String("ethContract", "", "Ethereum core bridge address for fetching current guardian set")
+	logLevel = QueryServerCmd.Flags().String("logLevel", "info", "Logging level (debug, info, warn, error, dpanic, panic, fatal)")
+	telemetryLokiURL = QueryServerCmd.Flags().String("telemetryLokiURL", "", "Loki cloud logging URL")
+	telemetryNodeName = QueryServerCmd.Flags().String("telemetryNodeName", "", "Node name used in telemetry")
+}
+
+var QueryServerCmd = &cobra.Command{
+	Use:   "query-server",
+	Short: "Run the cross-chain query server",
+	Run:   runQueryServer,
+}
+
+func runQueryServer(cmd *cobra.Command, args []string) {
+	common.SetRestrictiveUmask()
+
+	// Setup logging
+	lvl, err := ipfslog.LevelFromString(*logLevel)
+	if err != nil {
+		fmt.Println("Invalid log level")
+		os.Exit(1)
+	}
+
+	logger := ipfslog.Logger("query-server").Desugar()
+	ipfslog.SetAllLoggers(lvl)
+
+	if *telemetryLokiURL != "" {
+		logger.Info("Using Loki telemetry logger")
+		if *telemetryNodeName == "" {
+			logger.Fatal("if --telemetryLokiURL is specified --telemetryNodeName must be specified")
+		}
+		labels := map[string]string{
+			"network":   *p2pNetworkID,
+			"node_name": *telemetryNodeName,
+			"version":   version.Version(),
+		}
+
+		tm, err := telemetry.NewLokiCloudLogger(context.Background(), logger, *telemetryLokiURL, "ccq_server", true, labels)
+		if err != nil {
+			logger.Fatal("Failed to initialize telemetry", zap.Error(err))
+		}
+
+		defer tm.Close()
+		logger = tm.WrapLogger(logger) // Wrap logger with telemetry logger
+	}
+
+	env, err := common.ParseEnvironment(*envStr)
+	if err != nil || (env != common.UnsafeDevNet && env != common.TestNet && env != common.MainNet) {
+		if *envStr == "" {
+			logger.Fatal("Please specify --env")
+		}
+		logger.Fatal("Invalid value for --env, must be dev, test or prod", zap.String("val", *envStr))
+	}
+
+	if *p2pNetworkID == DEV_NETWORK_ID && env != common.UnsafeDevNet {
+		logger.Fatal("May not set --network to dev unless --env is also dev", zap.String("network", *p2pNetworkID), zap.String("env", *envStr))
+	}
+
+	// Verify flags
+	if *nodeKeyPath == "" {
+		logger.Fatal("Please specify --nodeKey")
+	}
+	if *p2pBootstrap == "" {
+		logger.Fatal("Please specify --bootstrap")
+	}
+	if *permFile == "" {
+		logger.Fatal("Please specify --permFile")
+	}
+	if *ethRPC == "" {
+		logger.Fatal("Please specify --ethRPC")
+	}
+	if *ethContract == "" {
+		logger.Fatal("Please specify --ethContract")
+	}
+
+	permissions, err := parseConfigFile(*permFile)
+	if err != nil {
+		logger.Fatal("Failed to load permissions file", zap.String("permFile", *permFile), zap.Error(err))
+	}
+
+	// Load p2p private key
+	var priv crypto.PrivKey
+	priv, err = common.GetOrCreateNodeKey(logger, *nodeKeyPath)
+	if err != nil {
+		logger.Fatal("Failed to load node key", zap.Error(err))
+	}
+
+	var signerKey *ecdsa.PrivateKey
+	if *signerKeyPath != "" {
+		signerKey, err = common.LoadArmoredKey(*signerKeyPath, CCQ_SERVER_SIGNING_KEY, false)
+		if err != nil {
+			logger.Fatal("Failed to loader signer key", zap.Error(err))
+		}
+
+		logger.Info("will sign unsigned requests if api key supports it", zap.Stringer("signingKey", ethCrypto.PubkeyToAddress(signerKey.PublicKey)))
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Run p2p
+	pendingResponses := NewPendingResponses()
+	p2p, err := runP2P(ctx, priv, *p2pPort, *p2pNetworkID, *p2pBootstrap, *ethRPC, *ethContract, pendingResponses, logger)
+	if err != nil {
+		logger.Fatal("Failed to start p2p", zap.Error(err))
+	}
+
+	// Start the HTTP server
+	go func() {
+		s := NewHTTPServer(*listenAddr, p2p.topic_req, permissions, signerKey, pendingResponses, logger, env)
+		logger.Sugar().Infof("Server listening on %s", *listenAddr)
+		err := s.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logger.Fatal("Server closed unexpectedly", zap.Error(err))
+		}
+	}()
+
+	<-ctx.Done()
+	logger.Info("Context cancelled, exiting...")
+
+	// Cleanly shutdown
+	// Without this the same host won't properly discover peers until some timeout
+	p2p.sub.Cancel()
+	if err := p2p.topic_req.Close(); err != nil {
+		logger.Error("Error closing the request topic", zap.Error(err))
+	}
+	if err := p2p.topic_resp.Close(); err != nil {
+		logger.Error("Error closing the response topic", zap.Error(err))
+	}
+	if err := p2p.host.Close(); err != nil {
+		logger.Error("Error closing the host", zap.Error(err))
+	}
+}

node/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/certusone/wormhole/node/cmd/ccq"
 	"github.com/certusone/wormhole/node/cmd/debug"
 	"github.com/certusone/wormhole/node/cmd/spy"
 	"github.com/certusone/wormhole/node/pkg/version"
@@ -48,6 +49,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.guardiand.yaml)")
 	rootCmd.AddCommand(guardiand.NodeCmd)
 	rootCmd.AddCommand(spy.SpyCmd)
+	rootCmd.AddCommand(ccq.QueryServerCmd)
 	rootCmd.AddCommand(guardiand.KeygenCmd)
 	rootCmd.AddCommand(guardiand.AdminCmd)
 	rootCmd.AddCommand(guardiand.TemplateCmd)

deltaswapio/deltaswap

(cherry picked from commit ad696d2f3cfc6e3f8ca889a7ac7ae3b40aa60dd8)

node/cmd/ccq/http.go
@@ -1,0 +1,179 @@
+package ccq
+
+import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sort"
+	"time"
+
+	"github.com/deltaswapio/deltaswap/node/pkg/common"
+	gossipv1 "github.com/deltaswapio/deltaswap/node/pkg/proto/gossip/v1"
+	"github.com/deltaswapio/deltaswap/node/pkg/query"
+	"github.com/gorilla/mux"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+)
+
+type queryRequest struct {
+	Bytes     string `json:"bytes"`
+	Signature string `json:"signature"`
+}
+
+type queryResponse struct {
+	Bytes      string   `json:"bytes"`
+	Signatures []string `json:"signatures"`
+}
+
+type httpServer struct {
+	topic            *pubsub.Topic
+	logger           *zap.Logger
+	env              common.Environment
+	permissions      Permissions
+	signerKey        *ecdsa.PrivateKey
+	pendingResponses *PendingResponses
+}
+
+func (s *httpServer) handleQuery(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for all requests.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+
+		w.Header().Set("Access-Control-Allow-Methods", "PUT, POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Api-Key")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	var q queryRequest
+	err := json.NewDecoder(r.Body).Decode(&q)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// There should be one and only one API key in the header.
+	apiKey, exists := r.Header["X-Api-Key"]
+	if !exists || len(apiKey) != 1 {
+		s.logger.Debug("received a request without an api key", zap.Stringer("url", r.URL), zap.Error(err))
+		http.Error(w, "api key is missing", http.StatusUnauthorized)
+		return
+	}
+
+	queryRequestBytes, err := hex.DecodeString(q.Bytes)
+	if err != nil {
+		s.logger.Debug("failed to decode request bytes", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	signature, err := hex.DecodeString(q.Signature)
+	if err != nil {
+		s.logger.Debug("failed to decode signature bytes", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	signedQueryRequest := &gossipv1.SignedQueryRequest{
+		QueryRequest: queryRequestBytes,
+		Signature:    signature,
+	}
+
+	if status, err := validateRequest(s.logger, s.env, s.permissions, s.signerKey, apiKey[0], signedQueryRequest); err != nil {
+		// Don't need to log here because the details were logged in the function.
+		http.Error(w, err.Error(), status)
+		return
+	}
+
+	m := gossipv1.GossipMessage{
+		Message: &gossipv1.GossipMessage_SignedQueryRequest{
+			SignedQueryRequest: signedQueryRequest,
+		},
+	}
+
+	b, err := proto.Marshal(&m)
+	if err != nil {
+		s.logger.Error("failed to marshal gossip message", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	pendingResponse := NewPendingResponse(signedQueryRequest)
+	added := s.pendingResponses.Add(pendingResponse)
+	if !added {
+		http.Error(w, "Duplicate request", http.StatusBadRequest)
+		return
+	}
+
+	err = s.topic.Publish(r.Context(), b)
+	if err != nil {
+		s.logger.Error("failed to publish gossip message", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		s.pendingResponses.Remove(pendingResponse)
+		return
+	}
+
+	// Wait for the response or timeout
+	select {
+	case <-time.After(query.RequestTimeout + 5*time.Second):
+		http.Error(w, "Timed out waiting for response", http.StatusGatewayTimeout)
+	case res := <-pendingResponse.ch:
+		resBytes, err := res.Response.Marshal()
+		if err != nil {
+			s.logger.Error("failed to marshal response", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			break
+		}
+		// Signature indices must be ascending for on-chain verification
+		sort.Slice(res.Signatures, func(i, j int) bool {
+			return res.Signatures[i].Index < res.Signatures[j].Index
+		})
+		signatures := make([]string, 0, len(res.Signatures))
+		for _, s := range res.Signatures {
+			// ECDSA signature + a byte for the index of the phylax in the phylax set
+			signature := fmt.Sprintf("%s%02x", s.Signature, uint8(s.Index))
+			signatures = append(signatures, signature)
+		}
+		w.Header().Add("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(&queryResponse{
+			Signatures: signatures,
+			Bytes:      hex.EncodeToString(resBytes),
+		})
+		if err != nil {
+			s.logger.Error("failed to encode response", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	s.pendingResponses.Remove(pendingResponse)
+}
+
+func (s *httpServer) handleHealth(w http.ResponseWriter, r *http.Request) {
+	s.logger.Debug("health check")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "ok")
+}
+
+func NewHTTPServer(addr string, t *pubsub.Topic, permissions Permissions, signerKey *ecdsa.PrivateKey, p *PendingResponses, logger *zap.Logger, env common.Environment) *http.Server {
+	s := &httpServer{
+		topic:            t,
+		permissions:      permissions,
+		signerKey:        signerKey,
+		pendingResponses: p,
+		logger:           logger,
+		env:              env,
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/query", s.handleQuery).Methods("PUT", "POST", "OPTIONS")
+	r.HandleFunc("/v1/health", s.handleHealth).Methods("GET")
+	return &http.Server{
+		Addr:              addr,
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+}

node/cmd/ccq/pending_request.go
@@ -1,0 +1,60 @@
+package ccq
+
+import (
+	"encoding/hex"
+	"sync"
+
+	gossipv1 "github.com/deltaswapio/deltaswap/node/pkg/proto/gossip/v1"
+)
+
+type PendingResponse struct {
+	req *gossipv1.SignedQueryRequest
+	ch  chan *SignedResponse
+}
+
+func NewPendingResponse(req *gossipv1.SignedQueryRequest) *PendingResponse {
+	return &PendingResponse{
+		req: req,
+		ch:  make(chan *SignedResponse),
+	}
+}
+
+type PendingResponses struct {
+	pendingResponses map[string]*PendingResponse
+	mu               sync.RWMutex
+}
+
+func NewPendingResponses() *PendingResponses {
+	return &PendingResponses{
+		pendingResponses: make(map[string]*PendingResponse),
+	}
+}
+
+func (p *PendingResponses) Add(r *PendingResponse) bool {
+	signature := hex.EncodeToString(r.req.Signature)
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if _, ok := p.pendingResponses[signature]; ok {
+		// the request w/ this signature is already being handled
+		// don't overwrite
+		return false
+	}
+	p.pendingResponses[signature] = r
+	return true
+}
+
+func (p *PendingResponses) Get(signature string) *PendingResponse {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	if r, ok := p.pendingResponses[signature]; ok {
+		return r
+	}
+	return nil
+}
+
+func (p *PendingResponses) Remove(r *PendingResponse) {
+	signature := hex.EncodeToString(r.req.Signature)
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	delete(p.pendingResponses, signature)
+}

node/cmd/ccq/query_server.go
@@ -1,0 +1,184 @@
+// Note: To generate a signer key file do: phylaxd keygen --block-type "CCQ SERVER SIGNING KEY" /path/to/key/file
+// You will need to add this key to ccqAllowedRequesters in the phylax configs.
+
+package ccq
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/deltaswapio/deltaswap/node/pkg/common"
+	"github.com/deltaswapio/deltaswap/node/pkg/telemetry"
+	"github.com/deltaswapio/deltaswap/node/pkg/version"
+	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	ipfslog "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+const CCQ_SERVER_SIGNING_KEY = "CCQ SERVER SIGNING KEY"
+
+var (
+	envStr            *string
+	p2pNetworkID      *string
+	p2pPort           *uint
+	p2pBootstrap      *string
+	listenAddr        *string
+	nodeKeyPath       *string
+	signerKeyPath     *string
+	permFile          *string
+	ethRPC            *string
+	ethContract       *string
+	logLevel          *string
+	telemetryLokiURL  *string
+	telemetryNodeName *string
+)
+
+const DEV_NETWORK_ID = "/wormhole/dev"
+
+func init() {
+	envStr = QueryServerCmd.Flags().String("env", "", "environment (dev, test, prod)")
+	p2pNetworkID = QueryServerCmd.Flags().String("network", DEV_NETWORK_ID, "P2P network identifier")
+	p2pPort = QueryServerCmd.Flags().Uint("port", 8995, "P2P UDP listener port")
+	p2pBootstrap = QueryServerCmd.Flags().String("bootstrap", "", "P2P bootstrap peers (comma-separated)")
+	nodeKeyPath = QueryServerCmd.Flags().String("nodeKey", "", "Path to node key (will be generated if it doesn't exist)")
+	signerKeyPath = QueryServerCmd.Flags().String("signerKey", "", "Path to key used to sign unsigned queries")
+	listenAddr = QueryServerCmd.Flags().String("listenAddr", "[::]:6069", "Listen address for query server (disabled if blank)")
+	permFile = QueryServerCmd.Flags().String("permFile", "", "JSON file containing permissions configuration")
+	ethRPC = QueryServerCmd.Flags().String("ethRPC", "", "Ethereum RPC for fetching current phylax set")
+	ethContract = QueryServerCmd.Flags().String("ethContract", "", "Ethereum core bridge address for fetching current phylax set")
+	logLevel = QueryServerCmd.Flags().String("logLevel", "info", "Logging level (debug, info, warn, error, dpanic, panic, fatal)")
+	telemetryLokiURL = QueryServerCmd.Flags().String("telemetryLokiURL", "", "Loki cloud logging URL")
+	telemetryNodeName = QueryServerCmd.Flags().String("telemetryNodeName", "", "Node name used in telemetry")
+}
+
+var QueryServerCmd = &cobra.Command{
+	Use:   "query-server",
+	Short: "Run the cross-chain query server",
+	Run:   runQueryServer,
+}
+
+func runQueryServer(cmd *cobra.Command, args []string) {
+	common.SetRestrictiveUmask()
+
+	// Setup logging
+	lvl, err := ipfslog.LevelFromString(*logLevel)
+	if err != nil {
+		fmt.Println("Invalid log level")
+		os.Exit(1)
+	}
+
+	logger := ipfslog.Logger("query-server").Desugar()
+	ipfslog.SetAllLoggers(lvl)
+
+	if *telemetryLokiURL != "" {
+		logger.Info("Using Loki telemetry logger")
+		if *telemetryNodeName == "" {
+			logger.Fatal("if --telemetryLokiURL is specified --telemetryNodeName must be specified")
+		}
+		labels := map[string]string{
+			"network":   *p2pNetworkID,
+			"node_name": *telemetryNodeName,
+			"version":   version.Version(),
+		}
+
+		tm, err := telemetry.NewLokiCloudLogger(context.Background(), logger, *telemetryLokiURL, "ccq_server", true, labels)
+		if err != nil {
+			logger.Fatal("Failed to initialize telemetry", zap.Error(err))
+		}
+
+		defer tm.Close()
+		logger = tm.WrapLogger(logger) // Wrap logger with telemetry logger
+	}
+
+	env, err := common.ParseEnvironment(*envStr)
+	if err != nil || (env != common.UnsafeDevNet && env != common.TestNet && env != common.MainNet) {
+		if *envStr == "" {
+			logger.Fatal("Please specify --env")
+		}
+		logger.Fatal("Invalid value for --env, must be dev, test or prod", zap.String("val", *envStr))
+	}
+
+	if *p2pNetworkID == DEV_NETWORK_ID && env != common.UnsafeDevNet {
+		logger.Fatal("May not set --network to dev unless --env is also dev", zap.String("network", *p2pNetworkID), zap.String("env", *envStr))
+	}
+
+	// Verify flags
+	if *nodeKeyPath == "" {
+		logger.Fatal("Please specify --nodeKey")
+	}
+	if *p2pBootstrap == "" {
+		logger.Fatal("Please specify --bootstrap")
+	}
+	if *permFile == "" {
+		logger.Fatal("Please specify --permFile")
+	}
+	if *ethRPC == "" {
+		logger.Fatal("Please specify --ethRPC")
+	}
+	if *ethContract == "" {
+		logger.Fatal("Please specify --ethContract")
+	}
+
+	permissions, err := parseConfigFile(*permFile)
+	if err != nil {
+		logger.Fatal("Failed to load permissions file", zap.String("permFile", *permFile), zap.Error(err))
+	}
+
+	// Load p2p private key
+	var priv crypto.PrivKey
+	priv, err = common.GetOrCreateNodeKey(logger, *nodeKeyPath)
+	if err != nil {
+		logger.Fatal("Failed to load node key", zap.Error(err))
+	}
+
+	var signerKey *ecdsa.PrivateKey
+	if *signerKeyPath != "" {
+		signerKey, err = common.LoadArmoredKey(*signerKeyPath, CCQ_SERVER_SIGNING_KEY, false)
+		if err != nil {
+			logger.Fatal("Failed to loader signer key", zap.Error(err))
+		}
+
+		logger.Info("will sign unsigned requests if api key supports it", zap.Stringer("signingKey", ethCrypto.PubkeyToAddress(signerKey.PublicKey)))
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Run p2p
+	pendingResponses := NewPendingResponses()
+	p2p, err := runP2P(ctx, priv, *p2pPort, *p2pNetworkID, *p2pBootstrap, *ethRPC, *ethContract, pendingResponses, logger)
+	if err != nil {
+		logger.Fatal("Failed to start p2p", zap.Error(err))
+	}
+
+	// Start the HTTP server
+	go func() {
+		s := NewHTTPServer(*listenAddr, p2p.topic_req, permissions, signerKey, pendingResponses, logger, env)
+		logger.Sugar().Infof("Server listening on %s", *listenAddr)
+		err := s.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logger.Fatal("Server closed unexpectedly", zap.Error(err))
+		}
+	}()
+
+	<-ctx.Done()
+	logger.Info("Context cancelled, exiting...")
+
+	// Cleanly shutdown
+	// Without this the same host won't properly discover peers until some timeout
+	p2p.sub.Cancel()
+	if err := p2p.topic_req.Close(); err != nil {
+		logger.Error("Error closing the request topic", zap.Error(err))
+	}
+	if err := p2p.topic_resp.Close(); err != nil {
+		logger.Error("Error closing the response topic", zap.Error(err))
+	}
+	if err := p2p.host.Close(); err != nil {
+		logger.Error("Error closing the host", zap.Error(err))
+	}
+}

node/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/deltaswapio/deltaswap/node/cmd/ccq"
 	"github.com/deltaswapio/deltaswap/node/cmd/debug"
 	"github.com/deltaswapio/deltaswap/node/cmd/spy"
 	"github.com/deltaswapio/deltaswap/node/pkg/version"
@@ -48,6 +49,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.phylaxd.yaml)")
 	rootCmd.AddCommand(phylaxd.NodeCmd)
 	rootCmd.AddCommand(spy.SpyCmd)
+	rootCmd.AddCommand(ccq.QueryServerCmd)
 	rootCmd.AddCommand(phylaxd.KeygenCmd)
 	rootCmd.AddCommand(phylaxd.AdminCmd)
 	rootCmd.AddCommand(phylaxd.TemplateCmd)