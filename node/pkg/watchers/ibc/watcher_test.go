friend413/Hole

NODE/IBC: Allow for a separate block height URL (#3480)

node/pkg/watchers/ibc/watcher_test.go
@@ -218,6 +218,8 @@ func TestParseIbcAllChannelChainsQueryResults(t *testing.T) {
 func TestConvertingWsUrlToHttpUrl(t *testing.T) {
 	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("ws://wormchain:26657/websocket"))
 	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("ws://wormchain:26657"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("wss://wormchain:26657/websocket"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("wss://wormchain:26657"))
 	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("wormchain:26657"))
 }
 

deltaswapio/deltaswap

(cherry picked from commit 3b17062869876d30b26a200bccab1838bd951773)

node/pkg/watchers/ibc/watcher_test.go
@@ -216,9 +216,11 @@ func TestParseIbcAllChannelChainsQueryResults(t *testing.T) {
 }
 
 func TestConvertingWsUrlToHttpUrl(t *testing.T) {
-	assert.Equal(t, "http://deltachain:26657", convertWsUrlToHttpUrl("ws://deltachain:26657/websocket"))
-	assert.Equal(t, "http://deltachain:26657", convertWsUrlToHttpUrl("ws://deltachain:26657"))
-	assert.Equal(t, "http://deltachain:26657", convertWsUrlToHttpUrl("deltachain:26657"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("ws://deltachain:26657/websocket"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("ws://deltachain:26657"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("wss://deltachain:26657/websocket"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("wss://deltachain:26657"))
+	assert.Equal(t, "http://wormchain:26657", convertWsUrlToHttpUrl("deltachain:26657"))
 }
 
 func TestParseAbciInfoResults(t *testing.T) {