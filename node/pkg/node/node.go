friend413/Hole

CCQ/Node: Guardian Changes (#3423)
* CCQ/Node: Guardian Changes

* Code review rework

node/pkg/node/node.go
@@ -11,7 +11,9 @@ import (
 	"github.com/certusone/wormhole/node/pkg/governor"
 	"github.com/certusone/wormhole/node/pkg/gwrelayer"
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+	"github.com/certusone/wormhole/node/pkg/query"
 	"github.com/certusone/wormhole/node/pkg/supervisor"
+	"github.com/wormhole-foundation/wormhole/sdk/vaa"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -59,6 +61,7 @@ type G struct {
 	acct            *accountant.Accountant
 	gov             *governor.ChainGovernor
 	gatewayRelayer  *gwrelayer.GatewayRelayer
+	queryHandler    *query.QueryHandler
 	publicrpcServer *grpc.Server
 
 	// runnables
@@ -82,6 +85,12 @@ type G struct {
 	obsvReqSendC channelPair[*gossipv1.ObservationRequest]
 	// acctC is the channel where messages will be put after they reached quorum in the accountant.
 	acctC channelPair[*common.MessagePublication]
+
+	// Cross Chain Query Handler channels
+	chainQueryReqC            map[vaa.ChainID]chan *query.PerChainQueryInternal
+	signedQueryReqC           channelPair[*gossipv1.SignedQueryRequest]
+	queryResponseC            channelPair[*query.PerChainQueryResponseInternal]
+	queryResponsePublicationC channelPair[*query.QueryResponsePublication]
 }
 
 func NewGuardianNode(
@@ -108,6 +117,11 @@ func (g *G) initializeBasic(rootCtxCancel context.CancelFunc) {
 	g.obsvReqC = makeChannelPair[*gossipv1.ObservationRequest](observationRequestInboundBufferSize)
 	g.obsvReqSendC = makeChannelPair[*gossipv1.ObservationRequest](observationRequestOutboundBufferSize)
 	g.acctC = makeChannelPair[*common.MessagePublication](accountant.MsgChannelCapacity)
+	// Cross Chain Query Handler channels
+	g.chainQueryReqC = make(map[vaa.ChainID]chan *query.PerChainQueryInternal)
+	g.signedQueryReqC = makeChannelPair[*gossipv1.SignedQueryRequest](query.SignedQueryRequestChannelSize)
+	g.queryResponseC = makeChannelPair[*query.PerChainQueryResponseInternal](0)
+	g.queryResponsePublicationC = makeChannelPair[*query.QueryResponsePublication](0)
 
 	// Guardian set state managed by processor
 	g.gst = common.NewGuardianSetState(nil)
@@ -188,6 +202,13 @@ func (g *G) Run(rootCtxCancel context.CancelFunc, options ...*GuardianOption) su
 			logger.Info("Starting gateway relayer")
 			if err := g.gatewayRelayer.Start(ctx); err != nil {
 				logger.Fatal("failed to start gateway relayer", zap.Error(err), zap.String("component", "gwrelayer"))
+			}
+		}
+
+		if g.queryHandler != nil {
+			logger.Info("Starting query handler", zap.String("component", "ccq"))
+			if err := g.queryHandler.Start(ctx); err != nil {
+				logger.Fatal("failed to create query handler", zap.Error(err), zap.String("component", "ccq"))
 			}
 		}
 

node/pkg/node/options.go
@@ -16,6 +16,7 @@ import (
 	"github.com/certusone/wormhole/node/pkg/p2p"
 	"github.com/certusone/wormhole/node/pkg/processor"
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+	"github.com/certusone/wormhole/node/pkg/query"
 	"github.com/certusone/wormhole/node/pkg/readiness"
 	"github.com/certusone/wormhole/node/pkg/supervisor"
 	"github.com/certusone/wormhole/node/pkg/watchers"
@@ -38,7 +39,7 @@ type GuardianOption struct {
 
 // GuardianOptionP2P configures p2p networking.
 // Dependencies: Accountant, Governor
-func GuardianOptionP2P(p2pKey libp2p_crypto.PrivKey, networkId string, bootstrapPeers string, nodeName string, disableHeartbeatVerify bool, port uint, ibcFeaturesFunc func() string) *GuardianOption {
+func GuardianOptionP2P(p2pKey libp2p_crypto.PrivKey, networkId string, bootstrapPeers string, nodeName string, disableHeartbeatVerify bool, port uint, ccqBootstrapPeers string, ccqPort uint, ccqAllowedPeers string, ibcFeaturesFunc func() string) *GuardianOption {
 	return &GuardianOption{
 		name:         "p2p",
 		dependencies: []string{"accountant", "governor", "gateway-relayer"},
@@ -72,6 +73,36 @@ func GuardianOptionP2P(p2pKey libp2p_crypto.PrivKey, networkId string, bootstrap
 				components,
 				ibcFeaturesFunc,
 				(g.gatewayRelayer != nil),
+				(g.queryHandler != nil),
+				g.signedQueryReqC.writeC,
+				g.queryResponsePublicationC.readC,
+				ccqBootstrapPeers,
+				ccqPort,
+				ccqAllowedPeers,
+			)
+
+			return nil
+		}}
+}
+
+// GuardianOptionQueryHandler configures the Cross Chain Query module.
+func GuardianOptionQueryHandler(ccqEnabled bool, allowedRequesters string) *GuardianOption {
+	return &GuardianOption{
+		name: "query",
+		f: func(ctx context.Context, logger *zap.Logger, g *G) error {
+			if !ccqEnabled {
+				logger.Info("ccq: cross chain query is disabled", zap.String("component", "ccq"))
+				return nil
+			}
+
+			g.queryHandler = query.NewQueryHandler(
+				logger,
+				g.env,
+				allowedRequesters,
+				g.signedQueryReqC.readC,
+				g.chainQueryReqC,
+				g.queryResponseC.readC,
+				g.queryResponsePublicationC.writeC,
 			)
 
 			return nil
@@ -301,6 +332,31 @@ func GuardianOptionWatchers(watcherConfigs []watchers.WatcherConfig, ibcWatcherC
 				}(chainMsgC[chainId], chainId)
 			}
 
+			// Per-chain query response channel
+			chainQueryResponseC := make(map[vaa.ChainID]chan *query.PerChainQueryResponseInternal)
+			// aggregate per-chain msgC into msgC.
+			// SECURITY defense-in-depth: This way we enforce that a watcher must set the msg.EmitterChain to its chainId, which makes the code easier to audit
+			for _, chainId := range vaa.GetAllNetworkIDs() {
+				chainQueryResponseC[chainId] = make(chan *query.PerChainQueryResponseInternal)
+				go func(c <-chan *query.PerChainQueryResponseInternal, chainId vaa.ChainID) {
+					for {
+						select {
+						case <-ctx.Done():
+							return
+						case response := <-c:
+							if response.ChainId != chainId {
+								// SECURITY: This should never happen. If it does, a watcher has been compromised.
+								logger.Fatal("SECURITY CRITICAL: Received query response from a chain that was not marked as originating from that chain",
+									zap.Uint16("responseChainId", uint16(response.ChainId)),
+									zap.Stringer("watcherChainId", chainId),
+								)
+							}
+							g.queryResponseC.writeC <- response
+						}
+					}
+				}(chainQueryResponseC[chainId], chainId)
+			}
+
 			watchers := make(map[watchers.NetworkID]interfaces.L1Finalizer)
 
 			for _, wc := range watcherConfigs {
@@ -316,6 +372,7 @@ func GuardianOptionWatchers(watcherConfigs []watchers.WatcherConfig, ibcWatcherC
 				}
 
 				chainObsvReqC[wc.GetChainID()] = make(chan *gossipv1.ObservationRequest, observationRequestPerChainBufferSize)
+				g.chainQueryReqC[wc.GetChainID()] = make(chan *query.PerChainQueryInternal, query.QueryRequestBufferSize)
 
 				if wc.RequiredL1Finalizer() != "" {
 					l1watcher, ok := watchers[wc.RequiredL1Finalizer()]
@@ -327,7 +384,7 @@ func GuardianOptionWatchers(watcherConfigs []watchers.WatcherConfig, ibcWatcherC
 					wc.SetL1Finalizer(l1watcher)
 				}
 
-				l1finalizer, runnable, err := wc.Create(chainMsgC[wc.GetChainID()], chainObsvReqC[wc.GetChainID()], g.setC.writeC, g.env)
+				l1finalizer, runnable, err := wc.Create(chainMsgC[wc.GetChainID()], chainObsvReqC[wc.GetChainID()], g.chainQueryReqC[wc.GetChainID()], chainQueryResponseC[wc.GetChainID()], g.setC.writeC, g.env)
 
 				if err != nil {
 					return fmt.Errorf("error creating watcher: %w", err)

node/pkg/watchers/cosmwasm/config.go
@@ -3,6 +3,7 @@ package cosmwasm
 import (
 	"github.com/certusone/wormhole/node/pkg/common"
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
+	"github.com/certusone/wormhole/node/pkg/query"
 	"github.com/certusone/wormhole/node/pkg/supervisor"
 	"github.com/certusone/wormhole/node/pkg/watchers"
 	"github.com/certusone/wormhole/node/pkg/watchers/interfaces"
@@ -36,6 +37,8 @@ func (wc *WatcherConfig) SetL1Finalizer(l1finalizer interfaces.L1Finalizer) {
 func (wc *WatcherConfig) Create(
 	msgC chan<- *common.MessagePublication,
 	obsvReqC <-chan *gossipv1.ObservationRequest,
+	_ <-chan *query.PerChainQueryInternal,
+	_ chan<- *query.PerChainQueryResponseInternal,
 	_ chan<- *common.GuardianSet,
 	env common.Environment,
 ) (interfaces.L1Finalizer, supervisor.Runnable, error) {

deltaswapio/deltaswap

(cherry picked from commit 669e2bc40e93f0f0524932d74339a6c783a33d9f)

node/pkg/node/node.go
@@ -11,7 +11,9 @@ import (
 	"github.com/deltaswapio/deltaswap/node/pkg/governor"
 	"github.com/deltaswapio/deltaswap/node/pkg/gwrelayer"
 	gossipv1 "github.com/deltaswapio/deltaswap/node/pkg/proto/gossip/v1"
+	"github.com/deltaswapio/deltaswap/node/pkg/query"
 	"github.com/deltaswapio/deltaswap/node/pkg/supervisor"
+	"github.com/deltaswapio/deltaswap/sdk/vaa"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -59,6 +61,7 @@ type G struct {
 	acct            *accountant.Accountant
 	gov             *governor.ChainGovernor
 	gatewayRelayer  *gwrelayer.GatewayRelayer
+	queryHandler    *query.QueryHandler
 	publicrpcServer *grpc.Server
 
 	// runnables
@@ -82,6 +85,12 @@ type G struct {
 	obsvReqSendC channelPair[*gossipv1.ObservationRequest]
 	// acctC is the channel where messages will be put after they reached quorum in the accountant.
 	acctC channelPair[*common.MessagePublication]
+
+	// Cross Chain Query Handler channels
+	chainQueryReqC            map[vaa.ChainID]chan *query.PerChainQueryInternal
+	signedQueryReqC           channelPair[*gossipv1.SignedQueryRequest]
+	queryResponseC            channelPair[*query.PerChainQueryResponseInternal]
+	queryResponsePublicationC channelPair[*query.QueryResponsePublication]
 }
 
 func NewPhylaxNode(
@@ -108,6 +117,11 @@ func (g *G) initializeBasic(rootCtxCancel context.CancelFunc) {
 	g.obsvReqC = makeChannelPair[*gossipv1.ObservationRequest](observationRequestInboundBufferSize)
 	g.obsvReqSendC = makeChannelPair[*gossipv1.ObservationRequest](observationRequestOutboundBufferSize)
 	g.acctC = makeChannelPair[*common.MessagePublication](accountant.MsgChannelCapacity)
+	// Cross Chain Query Handler channels
+	g.chainQueryReqC = make(map[vaa.ChainID]chan *query.PerChainQueryInternal)
+	g.signedQueryReqC = makeChannelPair[*gossipv1.SignedQueryRequest](query.SignedQueryRequestChannelSize)
+	g.queryResponseC = makeChannelPair[*query.PerChainQueryResponseInternal](0)
+	g.queryResponsePublicationC = makeChannelPair[*query.QueryResponsePublication](0)
 
 	// Phylax set state managed by processor
 	g.gst = common.NewPhylaxSetState(nil)
@@ -188,6 +202,13 @@ func (g *G) Run(rootCtxCancel context.CancelFunc, options ...*PhylaxOption) supe
 			logger.Info("Starting gateway relayer")
 			if err := g.gatewayRelayer.Start(ctx); err != nil {
 				logger.Fatal("failed to start gateway relayer", zap.Error(err), zap.String("component", "gwrelayer"))
+			}
+		}
+
+		if g.queryHandler != nil {
+			logger.Info("Starting query handler", zap.String("component", "ccq"))
+			if err := g.queryHandler.Start(ctx); err != nil {
+				logger.Fatal("failed to create query handler", zap.Error(err), zap.String("component", "ccq"))
 			}
 		}
 

node/pkg/node/options.go
@@ -17,6 +17,7 @@ import (
 	"github.com/deltaswapio/deltaswap/node/pkg/p2p"
 	"github.com/deltaswapio/deltaswap/node/pkg/processor"
 	gossipv1 "github.com/deltaswapio/deltaswap/node/pkg/proto/gossip/v1"
+	"github.com/deltaswapio/deltaswap/node/pkg/query"
 	"github.com/deltaswapio/deltaswap/node/pkg/readiness"
 	"github.com/deltaswapio/deltaswap/node/pkg/supervisor"
 	"github.com/deltaswapio/deltaswap/node/pkg/watchers"
@@ -38,7 +39,7 @@ type PhylaxOption struct {
 
 // PhylaxOptionP2P configures p2p networking.
 // Dependencies: Accountant, Governor
-func PhylaxOptionP2P(p2pKey libp2p_crypto.PrivKey, networkId string, bootstrapPeers string, nodeName string, disableHeartbeatVerify bool, port uint, ibcFeaturesFunc func() string) *PhylaxOption {
+func PhylaxOptionP2P(p2pKey libp2p_crypto.PrivKey, networkId string, bootstrapPeers string, nodeName string, disableHeartbeatVerify bool, port uint, ccqBootstrapPeers string, ccqPort uint, ccqAllowedPeers string, ibcFeaturesFunc func() string) *PhylaxOption {
 	return &PhylaxOption{
 		name:         "p2p",
 		dependencies: []string{"accountant", "governor", "gateway-relayer"},
@@ -72,6 +73,36 @@ func PhylaxOptionP2P(p2pKey libp2p_crypto.PrivKey, networkId string, bootstrapPe
 				components,
 				ibcFeaturesFunc,
 				(g.gatewayRelayer != nil),
+				(g.queryHandler != nil),
+				g.signedQueryReqC.writeC,
+				g.queryResponsePublicationC.readC,
+				ccqBootstrapPeers,
+				ccqPort,
+				ccqAllowedPeers,
+			)
+
+			return nil
+		}}
+}
+
+// PhylaxOptionQueryHandler configures the Cross Chain Query module.
+func PhylaxOptionQueryHandler(ccqEnabled bool, allowedRequesters string) *PhylaxOption {
+	return &PhylaxOption{
+		name: "query",
+		f: func(ctx context.Context, logger *zap.Logger, g *G) error {
+			if !ccqEnabled {
+				logger.Info("ccq: cross chain query is disabled", zap.String("component", "ccq"))
+				return nil
+			}
+
+			g.queryHandler = query.NewQueryHandler(
+				logger,
+				g.env,
+				allowedRequesters,
+				g.signedQueryReqC.readC,
+				g.chainQueryReqC,
+				g.queryResponseC.readC,
+				g.queryResponsePublicationC.writeC,
 			)
 
 			return nil
@@ -301,6 +332,31 @@ func PhylaxOptionWatchers(watcherConfigs []watchers.WatcherConfig, ibcWatcherCon
 				}(chainMsgC[chainId], chainId)
 			}
 
+			// Per-chain query response channel
+			chainQueryResponseC := make(map[vaa.ChainID]chan *query.PerChainQueryResponseInternal)
+			// aggregate per-chain msgC into msgC.
+			// SECURITY defense-in-depth: This way we enforce that a watcher must set the msg.EmitterChain to its chainId, which makes the code easier to audit
+			for _, chainId := range vaa.GetAllNetworkIDs() {
+				chainQueryResponseC[chainId] = make(chan *query.PerChainQueryResponseInternal)
+				go func(c <-chan *query.PerChainQueryResponseInternal, chainId vaa.ChainID) {
+					for {
+						select {
+						case <-ctx.Done():
+							return
+						case response := <-c:
+							if response.ChainId != chainId {
+								// SECURITY: This should never happen. If it does, a watcher has been compromised.
+								logger.Fatal("SECURITY CRITICAL: Received query response from a chain that was not marked as originating from that chain",
+									zap.Uint16("responseChainId", uint16(response.ChainId)),
+									zap.Stringer("watcherChainId", chainId),
+								)
+							}
+							g.queryResponseC.writeC <- response
+						}
+					}
+				}(chainQueryResponseC[chainId], chainId)
+			}
+
 			watchers := make(map[watchers.NetworkID]interfaces.L1Finalizer)
 
 			for _, wc := range watcherConfigs {
@@ -316,6 +372,7 @@ func PhylaxOptionWatchers(watcherConfigs []watchers.WatcherConfig, ibcWatcherCon
 				}
 
 				chainObsvReqC[wc.GetChainID()] = make(chan *gossipv1.ObservationRequest, observationRequestPerChainBufferSize)
+				g.chainQueryReqC[wc.GetChainID()] = make(chan *query.PerChainQueryInternal, query.QueryRequestBufferSize)
 
 				if wc.RequiredL1Finalizer() != "" {
 					l1watcher, ok := watchers[wc.RequiredL1Finalizer()]
@@ -327,7 +384,7 @@ func PhylaxOptionWatchers(watcherConfigs []watchers.WatcherConfig, ibcWatcherCon
 					wc.SetL1Finalizer(l1watcher)
 				}
 
-				l1finalizer, runnable, err := wc.Create(chainMsgC[wc.GetChainID()], chainObsvReqC[wc.GetChainID()], g.setC.writeC, g.env)
+				l1finalizer, runnable, err := wc.Create(chainMsgC[wc.GetChainID()], chainObsvReqC[wc.GetChainID()], g.chainQueryReqC[wc.GetChainID()], chainQueryResponseC[wc.GetChainID()], g.setC.writeC, g.env)
 
 				if err != nil {
 					return fmt.Errorf("error creating watcher: %w", err)

node/pkg/watchers/cosmwasm/config.go
@@ -3,6 +3,7 @@ package cosmwasm
 import (
 	"github.com/deltaswapio/deltaswap/node/pkg/common"
 	gossipv1 "github.com/deltaswapio/deltaswap/node/pkg/proto/gossip/v1"
+	"github.com/deltaswapio/deltaswap/node/pkg/query"
 	"github.com/deltaswapio/deltaswap/node/pkg/supervisor"
 	"github.com/deltaswapio/deltaswap/node/pkg/watchers"
 	"github.com/deltaswapio/deltaswap/node/pkg/watchers/interfaces"
@@ -36,6 +37,8 @@ func (wc *WatcherConfig) SetL1Finalizer(l1finalizer interfaces.L1Finalizer) {
 func (wc *WatcherConfig) Create(
 	msgC chan<- *common.MessagePublication,
 	obsvReqC <-chan *gossipv1.ObservationRequest,
+	_ <-chan *query.PerChainQueryInternal,
+	_ chan<- *query.PerChainQueryResponseInternal,
 	_ chan<- *common.PhylaxSet,
 	env common.Environment,
 ) (interfaces.L1Finalizer, supervisor.Runnable, error) {