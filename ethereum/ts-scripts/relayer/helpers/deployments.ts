cb-defi-notifs/wormhole-foundation-wormhole-637fcac6

evm: deployment script improvements.
- Adds overrides for polygon and fantom to use legacy tx envelopes to avoid bad gas price feeds in public JSON-RPC endpoints.
- Multiplies by a factor of `1.1` the gas estimated to prevent slightly skewed estimations from ruining the pipeline.
- Fixes gas price of BSC chains to 3 gwei.
- Adds the `LedgerSigner` class to sign with a ledger device.

ethereum/ts-scripts/relayer/helpers/deployments.ts
@@ -1,8 +1,11 @@
-import { DeliveryProviderProxy__factory } from "../../../ethers-contracts";
-import { DeliveryProviderSetup__factory } from "../../../ethers-contracts";
-import { DeliveryProviderImplementation__factory } from "../../../ethers-contracts";
-import { MockRelayerIntegration__factory } from "../../../ethers-contracts";
-import { WormholeRelayer__factory } from "../../../ethers-contracts";
+import {
+  DeliveryProviderProxy__factory,
+  DeliveryProviderSetup__factory,
+  DeliveryProviderImplementation__factory,
+  MockRelayerIntegration__factory,
+  WormholeRelayer__factory,
+  Create2Factory__factory,
+} from "../../../ethers-contracts";
 
 import {
   ChainInfo,
@@ -12,7 +15,6 @@ import {
   getCreate2Factory,
 } from "./env";
 import { ethers } from "ethers";
-import { Create2Factory__factory } from "../../../ethers-contracts";
 import { wait } from "./utils";
 
 export const setupContractSalt = Buffer.from("0xSetup");
@@ -22,42 +24,34 @@ export async function deployDeliveryProviderImplementation(
   chain: ChainInfo
 ): Promise<Deployment> {
   console.log("deployDeliveryProviderImplementation " + chain.chainId);
-  const signer = getSigner(chain);
+  const signer = await getSigner(chain);
+  const factory = new DeliveryProviderImplementation__factory(signer);
 
-  const contractInterface = DeliveryProviderImplementation__factory.createInterface();
-  const bytecode = DeliveryProviderImplementation__factory.bytecode;
-  //@ts-ignore
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
-  const contract = await factory.deploy();
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+  const overrides = await buildOverridesDeploy(factory, chain, []);
+  const contract = await factory.deploy(overrides);
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
 
 export async function deployDeliveryProviderSetup(
   chain: ChainInfo
 ): Promise<Deployment> {
   console.log("deployDeliveryProviderSetup " + chain.chainId);
-  const signer = getSigner(chain);
-  const contractInterface = DeliveryProviderSetup__factory.createInterface();
-  const bytecode = DeliveryProviderSetup__factory.bytecode;
-  //@ts-ignore
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
-  const contract = await factory.deploy();
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+
+  const signer = await getSigner(chain);
+  const factory = new DeliveryProviderSetup__factory(signer);
+
+  const overrides = await buildOverridesDeploy(factory, chain, []);
+  const contract = await factory.deploy(overrides);
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
+
+/**
+ * Deploys `DeliveryProvider` proxy with the old (account, nonce) tuple hashing creation mechanism.
+ */
 export async function deployDeliveryProviderProxy(
   chain: ChainInfo,
   deliveryProviderSetupAddress: string,
@@ -65,31 +59,27 @@ export async function deployDeliveryProviderProxy(
 ): Promise<Deployment> {
   console.log("deployDeliveryProviderProxy " + chain.chainId);
 
-  const signer = getSigner(chain);
-  const contractInterface = DeliveryProviderProxy__factory.createInterface();
-  const bytecode = DeliveryProviderProxy__factory.bytecode;
-  //@ts-ignore
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
+  const signer = await getSigner(chain);
+  const factory = new DeliveryProviderProxy__factory(signer);
 
-  let ABI = ["function setup(address,uint16)"];
-  let iface = new ethers.utils.Interface(ABI);
-  let encodedData = iface.encodeFunctionData("setup", [
+  const setupInterface = DeliveryProviderSetup__factory.createInterface();
+  const encodedData = setupInterface.encodeFunctionData("setup", [
     deliveryProviderImplementationAddress,
     chain.chainId,
   ]);
 
+  const overrides = await buildOverridesDeploy(factory, chain, [
+    deliveryProviderSetupAddress,
+    encodedData,
+  ]);
   const contract = await factory.deploy(
     deliveryProviderSetupAddress,
-    encodedData
+    encodedData,
+    overrides
   );
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
 
 export async function deployMockIntegration(
@@ -97,34 +87,41 @@ export async function deployMockIntegration(
 ): Promise<Deployment> {
   console.log("deployMockIntegration " + chain.chainId);
 
-  let signer = getSigner(chain);
-  const contractInterface = MockRelayerIntegration__factory.createInterface();
-  const bytecode = MockRelayerIntegration__factory.bytecode;
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
+  const signer = await getSigner(chain);
+  const factory = new MockRelayerIntegration__factory(signer);
+
+  const wormholeRelayerAddress = await getWormholeRelayerAddress(chain);
+  const overrides = await buildOverridesDeploy(factory, chain, [
+    chain.wormholeAddress,
+    wormholeRelayerAddress,
+  ]);
   const contract = await factory.deploy(
     chain.wormholeAddress,
-    await getWormholeRelayerAddress(chain)
+    wormholeRelayerAddress,
+    overrides
   );
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
 
+/**
+ * Deploys `Create2Factory` with the old (account, nonce) tuple hashing creation mechanism.
+ * To achieve same address multichain deployments, ensure that the
+ * same (address, nonce) tx pair creates the factory across all target chains.
+ */
 export async function deployCreate2Factory(
   chain: ChainInfo
 ): Promise<Deployment> {
   console.log("deployCreate2Factory " + chain.chainId);
 
-  const result = await new Create2Factory__factory(getSigner(chain))
-    .deploy()
-    .then(deployed);
-  console.log(`Successfully deployed contract at ${result.address}`);
-  return { address: result.address, chainId: chain.chainId };
+  const signer = await getSigner(chain);
+  const factory = new Create2Factory__factory(signer);
+
+  const overrides = await buildOverridesDeploy(factory, chain, []);
+  const contract = await factory.deploy(overrides).then(deployed);
+  console.log(`Successfully deployed contract at ${contract.address}`);
+  return { address: contract.address, chainId: chain.chainId };
 }
 
 export async function deployWormholeRelayerImplementation(
@@ -132,14 +129,23 @@ export async function deployWormholeRelayerImplementation(
 ): Promise<Deployment> {
   console.log("deployWormholeRelayerImplementation " + chain.chainId);
 
-  const result = await new WormholeRelayer__factory(getSigner(chain))
-    .deploy(chain.wormholeAddress)
+  const signer = await getSigner(chain);
+  const factory = new WormholeRelayer__factory(signer);
+
+  const overrides = await buildOverridesDeploy(factory, chain, [
+    chain.wormholeAddress,
+  ]);
+  const result = await new WormholeRelayer__factory(signer)
+    .deploy(chain.wormholeAddress, overrides)
     .then(deployed);
 
   console.log("Successfully deployed contract at " + result.address);
   return { address: result.address, chainId: chain.chainId };
 }
 
+/**
+ * Deploys `WormholeRelayer` proxy with the CREATE2 factory.
+ */
 export async function deployWormholeRelayerProxy(
   chain: ChainInfo,
   coreRelayerImplementationAddress: string,
@@ -147,14 +153,28 @@ export async function deployWormholeRelayerProxy(
 ): Promise<Deployment> {
   console.log("deployWormholeRelayerProxy " + chain.chainId);
 
-  const create2Factory = getCreate2Factory(chain);
+  const create2Factory = await getCreate2Factory(chain);
 
   const initData = WormholeRelayer__factory.createInterface().encodeFunctionData(
     "initialize",
     [ethers.utils.getAddress(defaultDeliveryProvider)]
   );
+  const overrides = await buildOverrides(
+    () =>
+      create2Factory.estimateGas.create2Proxy(
+        proxyContractSalt,
+        coreRelayerImplementationAddress,
+        initData
+      ),
+    chain
+  );
   const rx = await create2Factory
-    .create2Proxy(proxyContractSalt, coreRelayerImplementationAddress, initData)
+    .create2Proxy(
+      proxyContractSalt,
+      coreRelayerImplementationAddress,
+      initData,
+      overrides
+    )
     .then(wait);
 
   let proxyAddress: string;
@@ -166,8 +186,9 @@ export async function deployWormholeRelayerProxy(
       }
     } catch (e) {}
   }
+  const signer = await getSigner(chain);
   const computedAddr = await create2Factory.computeProxyAddress(
-    getSigner(chain).address,
+    await signer.getAddress(),
     proxyContractSalt
   );
   if (proxyAddress! !== computedAddr) {
@@ -179,3 +200,46 @@ export async function deployWormholeRelayerProxy(
 }
 
 const deployed = (x: ethers.Contract) => x.deployed();
+
+const estimateGasDeploy = async (
+  factory: ethers.ContractFactory,
+  args: unknown[]
+): Promise<ethers.BigNumber> => {
+  const deployTxArgs = factory.getDeployTransaction(...args);
+  return factory.signer.estimateGas(deployTxArgs);
+};
+
+const buildOverridesDeploy = async (
+  factory: ethers.ContractFactory,
+  chain: ChainInfo,
+  args: unknown[]
+): Promise<ethers.Overrides> => {
+  return buildOverrides(() => estimateGasDeploy(factory, args), chain);
+};
+
+async function overshootEstimationGas(
+  estimate: () => Promise<ethers.BigNumber>
+): Promise<ethers.BigNumber> {
+  const gasEstimate = await estimate();
+  // we multiply gas estimation by a factor 1.1 to avoid slightly skewed estimations from breaking transactions.
+  return gasEstimate.mul(1100).div(1000);
+}
+
+export async function buildOverrides(
+  estimate: () => Promise<ethers.BigNumber>,
+  chain: ChainInfo
+): Promise<ethers.Overrides> {
+  const overrides: ethers.Overrides = {
+    gasLimit: await overshootEstimationGas(estimate),
+  };
+  // If this is Polygon or Fantom, use the legacy tx envelope to avoid bad gas price feeds.
+  if (chain.chainId === 5 || chain.chainId === 10) {
+    overrides.type = 0;
+  } else if (chain.chainId === 4) {
+    // This is normally autodetected in bsc but we want to set the gas price to a fixed value.
+    // We need to ensure we are using the correct tx envelope in that case.
+    overrides.type = 0;
+    overrides.gasPrice = ethers.utils.parseUnits("3", "gwei");
+  }
+  return overrides;
+}

deltaswapio/deltaswap

ethereum/ts-scripts/relayer/helpers/deployments.ts
@@ -2,7 +2,7 @@ import { DeliveryProviderProxy__factory } from "../../../ethers-contracts";
 import { DeliveryProviderSetup__factory } from "../../../ethers-contracts";
 import { DeliveryProviderImplementation__factory } from "../../../ethers-contracts";
 import { MockRelayerIntegration__factory } from "../../../ethers-contracts";
-import { DeltaswapRelayer__factory } from "../../../ethers-contracts";
+import { DeltaswapRelayer__factory,  Create2Factory__factory} from "../../../ethers-contracts";
 
 import {
   ChainInfo,
@@ -12,7 +12,6 @@ import {
   getCreate2Factory,
 } from "./env";
 import { ethers } from "ethers";
-import { Create2Factory__factory } from "../../../ethers-contracts";
 import { wait } from "./utils";
 
 export const setupContractSalt = Buffer.from("0xSetup");
@@ -22,42 +21,34 @@ export async function deployDeliveryProviderImplementation(
   chain: ChainInfo
 ): Promise<Deployment> {
   console.log("deployDeliveryProviderImplementation " + chain.chainId);
-  const signer = getSigner(chain);
+  const signer = await getSigner(chain);
+  const factory = new DeliveryProviderImplementation__factory(signer);
 
-  const contractInterface = DeliveryProviderImplementation__factory.createInterface();
-  const bytecode = DeliveryProviderImplementation__factory.bytecode;
-  //@ts-ignore
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
-  const contract = await factory.deploy();
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+  const overrides = await buildOverridesDeploy(factory, chain, []);
+  const contract = await factory.deploy(overrides);
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
 
 export async function deployDeliveryProviderSetup(
   chain: ChainInfo
 ): Promise<Deployment> {
   console.log("deployDeliveryProviderSetup " + chain.chainId);
-  const signer = getSigner(chain);
-  const contractInterface = DeliveryProviderSetup__factory.createInterface();
-  const bytecode = DeliveryProviderSetup__factory.bytecode;
-  //@ts-ignore
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
-  const contract = await factory.deploy();
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+
+  const signer = await getSigner(chain);
+  const factory = new DeliveryProviderSetup__factory(signer);
+
+  const overrides = await buildOverridesDeploy(factory, chain, []);
+  const contract = await factory.deploy(overrides);
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
+
+/**
+ * Deploys `DeliveryProvider` proxy with the old (account, nonce) tuple hashing creation mechanism.
+ */
 export async function deployDeliveryProviderProxy(
   chain: ChainInfo,
   deliveryProviderSetupAddress: string,
@@ -65,31 +56,27 @@ export async function deployDeliveryProviderProxy(
 ): Promise<Deployment> {
   console.log("deployDeliveryProviderProxy " + chain.chainId);
 
-  const signer = getSigner(chain);
-  const contractInterface = DeliveryProviderProxy__factory.createInterface();
-  const bytecode = DeliveryProviderProxy__factory.bytecode;
-  //@ts-ignore
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
+  const signer = await getSigner(chain);
+  const factory = new DeliveryProviderProxy__factory(signer);
 
-  let ABI = ["function setup(address,uint16)"];
-  let iface = new ethers.utils.Interface(ABI);
-  let encodedData = iface.encodeFunctionData("setup", [
+  const setupInterface = DeliveryProviderSetup__factory.createInterface();
+  const encodedData = setupInterface.encodeFunctionData("setup", [
     deliveryProviderImplementationAddress,
     chain.chainId,
   ]);
 
+  const overrides = await buildOverridesDeploy(factory, chain, [
+    deliveryProviderSetupAddress,
+    encodedData,
+  ]);
   const contract = await factory.deploy(
     deliveryProviderSetupAddress,
-    encodedData
+    encodedData,
+    overrides
   );
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
 
 export async function deployMockIntegration(
@@ -97,34 +84,41 @@ export async function deployMockIntegration(
 ): Promise<Deployment> {
   console.log("deployMockIntegration " + chain.chainId);
 
-  let signer = getSigner(chain);
-  const contractInterface = MockRelayerIntegration__factory.createInterface();
-  const bytecode = MockRelayerIntegration__factory.bytecode;
-  const factory = new ethers.ContractFactory(
-    contractInterface,
-    bytecode,
-    signer
-  );
+  const signer = await getSigner(chain);
+  const factory = new MockRelayerIntegration__factory(signer);
+
+  const deltaswapRelayerAddress = await getDeltaswapRelayerAddress(chain);
+  const overrides = await buildOverridesDeploy(factory, chain, [
+    chain.deltaswapAddress,
+      deltaswapRelayerAddress,
+  ]);
   const contract = await factory.deploy(
     chain.deltaswapAddress,
-    await getDeltaswapRelayerAddress(chain)
+    deltaswapRelayerAddress,
+    overrides
   );
-  return await contract.deployed().then((result) => {
-    console.log("Successfully deployed contract at " + result.address);
-    return { address: result.address, chainId: chain.chainId };
-  });
+  const receipt = await contract.deployTransaction.wait();
+  console.log("Successfully deployed contract at " + receipt.contractAddress);
+  return { address: receipt.contractAddress, chainId: chain.chainId };
 }
 
+/**
+ * Deploys `Create2Factory` with the old (account, nonce) tuple hashing creation mechanism.
+ * To achieve same address multichain deployments, ensure that the
+ * same (address, nonce) tx pair creates the factory across all target chains.
+ */
 export async function deployCreate2Factory(
   chain: ChainInfo
 ): Promise<Deployment> {
   console.log("deployCreate2Factory " + chain.chainId);
 
-  const result = await new Create2Factory__factory(getSigner(chain))
-    .deploy()
-    .then(deployed);
-  console.log(`Successfully deployed contract at ${result.address}`);
-  return { address: result.address, chainId: chain.chainId };
+  const signer = await getSigner(chain);
+  const factory = new Create2Factory__factory(signer);
+
+  const overrides = await buildOverridesDeploy(factory, chain, []);
+  const contract = await factory.deploy(overrides).then(deployed);
+  console.log(`Successfully deployed contract at ${contract.address}`);
+  return { address: contract.address, chainId: chain.chainId };
 }
 
 export async function deployDeltaswapRelayerImplementation(
@@ -134,12 +128,23 @@ export async function deployDeltaswapRelayerImplementation(
 
   const result = await new DeltaswapRelayer__factory(getSigner(chain))
     .deploy(chain.deltaswapAddress)
+  const signer = await getSigner(chain);
+  const factory = new DeltaswapRelayer__factory(signer);
+
+  const overrides = await buildOverridesDeploy(factory, chain, [
+    chain.deltaswapAddress,
+  ]);
+  const result = await new DeltaswapRelayer__factory(signer)
+    .deploy(chain.deltaswapAddress, overrides)
     .then(deployed);
 
   console.log("Successfully deployed contract at " + result.address);
   return { address: result.address, chainId: chain.chainId };
 }
 
+/**
+ * Deploys `DeltaswapRelayer` proxy with the CREATE2 factory.
+ */
 export async function deployDeltaswapRelayerProxy(
   chain: ChainInfo,
   coreRelayerImplementationAddress: string,
@@ -147,14 +152,28 @@ export async function deployDeltaswapRelayerProxy(
 ): Promise<Deployment> {
   console.log("deployDeltaswapRelayerProxy " + chain.chainId);
 
-  const create2Factory = getCreate2Factory(chain);
+  const create2Factory = await getCreate2Factory(chain);
 
   const initData = DeltaswapRelayer__factory.createInterface().encodeFunctionData(
     "initialize",
     [ethers.utils.getAddress(defaultDeliveryProvider)]
   );
+  const overrides = await buildOverrides(
+    () =>
+      create2Factory.estimateGas.create2Proxy(
+        proxyContractSalt,
+        coreRelayerImplementationAddress,
+        initData
+      ),
+    chain
+  );
   const rx = await create2Factory
-    .create2Proxy(proxyContractSalt, coreRelayerImplementationAddress, initData)
+    .create2Proxy(
+      proxyContractSalt,
+      coreRelayerImplementationAddress,
+      initData,
+      overrides
+    )
     .then(wait);
 
   let proxyAddress: string;
@@ -166,8 +185,9 @@ export async function deployDeltaswapRelayerProxy(
       }
     } catch (e) {}
   }
+  const signer = await getSigner(chain);
   const computedAddr = await create2Factory.computeProxyAddress(
-    getSigner(chain).address,
+    await signer.getAddress(),
     proxyContractSalt
   );
   if (proxyAddress! !== computedAddr) {
@@ -179,3 +199,46 @@ export async function deployDeltaswapRelayerProxy(
 }
 
 const deployed = (x: ethers.Contract) => x.deployed();
+
+const estimateGasDeploy = async (
+  factory: ethers.ContractFactory,
+  args: unknown[]
+): Promise<ethers.BigNumber> => {
+  const deployTxArgs = factory.getDeployTransaction(...args);
+  return factory.signer.estimateGas(deployTxArgs);
+};
+
+const buildOverridesDeploy = async (
+  factory: ethers.ContractFactory,
+  chain: ChainInfo,
+  args: unknown[]
+): Promise<ethers.Overrides> => {
+  return buildOverrides(() => estimateGasDeploy(factory, args), chain);
+};
+
+async function overshootEstimationGas(
+  estimate: () => Promise<ethers.BigNumber>
+): Promise<ethers.BigNumber> {
+  const gasEstimate = await estimate();
+  // we multiply gas estimation by a factor 1.1 to avoid slightly skewed estimations from breaking transactions.
+  return gasEstimate.mul(1100).div(1000);
+}
+
+export async function buildOverrides(
+  estimate: () => Promise<ethers.BigNumber>,
+  chain: ChainInfo
+): Promise<ethers.Overrides> {
+  const overrides: ethers.Overrides = {
+    gasLimit: await overshootEstimationGas(estimate),
+  };
+  // If this is Polygon or Fantom, use the legacy tx envelope to avoid bad gas price feeds.
+  if (chain.chainId === 5 || chain.chainId === 10) {
+    overrides.type = 0;
+  } else if (chain.chainId === 4) {
+    // This is normally autodetected in bsc but we want to set the gas price to a fixed value.
+    // We need to ensure we are using the correct tx envelope in that case.
+    overrides.type = 0;
+    overrides.gasPrice = ethers.utils.parseUnits("3", "gwei");
+  }
+  return overrides;
+}