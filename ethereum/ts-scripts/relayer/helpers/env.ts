zeenden52u/wormhole

gRelayer: removes wormhole relayer address calculation with create2 factory.
Performing this calculation requires us to know the deployer of the
wormhole relayer contract. This could vary in the current deployment
scheme. Thus, we rely on the recorded wormhole relayer address instead.

ethereum/ts-scripts/relayer/helpers/env.ts
@@ -13,7 +13,6 @@ import {
   Create2Factory,
   Create2Factory__factory,
 } from "../../../ethers-contracts";
-import { proxyContractSalt } from "./deployments";
 
 export type ChainInfo = {
   evmNetworkId: number;
@@ -392,41 +391,23 @@ export async function getDeliveryProvider(
   return contract;
 }
 
-const wormholeRelayerAddressesCache: Partial<Record<ChainId, string>> = {};
 export async function getWormholeRelayerAddress(
   chain: ChainInfo,
-  forceCalculate?: boolean,
 ): Promise<string> {
   // See if we are in dev mode (i.e. forge contracts compiled without via-ir)
   const dev = get_env_var("DEV") == "True" ? true : false;
 
-  const contracts = readContracts();
-  //If useLastRun is false, then we want to bypass the calculations and just use what the contracts file says.
-  if (!contracts.useLastRun && !lastRunOverride && !forceCalculate) {
-    const thisChainsRelayer = loadWormholeRelayers(dev).find(
-      (x) => x.chainId == chain.chainId,
-    )?.address;
-    if (thisChainsRelayer) {
-      return thisChainsRelayer;
-    } else {
-      throw Error(
-        "Failed to find a WormholeRelayer contract address on chain " +
-          chain.chainId,
-      );
-    }
+  const thisChainsRelayer = loadWormholeRelayers(dev).find(
+    (x) => x.chainId == chain.chainId,
+  )?.address;
+  if (thisChainsRelayer) {
+    return thisChainsRelayer;
+  } else {
+    throw Error(
+      "Failed to find a WormholeRelayer contract address on chain " +
+      chain.chainId,
+    );
   }
-
-  if (!wormholeRelayerAddressesCache[chain.chainId]) {
-    const create2Factory = await getCreate2Factory(chain);
-    const signer = await getSigner(chain);
-    const address = await signer.getAddress();
-
-    wormholeRelayerAddressesCache[
-      chain.chainId
-    ] = await create2Factory.computeProxyAddress(address, proxyContractSalt);
-  }
-
-  return wormholeRelayerAddressesCache[chain.chainId]!;
 }
 
 export async function getWormholeRelayer(

deltaswapio/deltaswap

ethereum/ts-scripts/relayer/helpers/env.ts
@@ -13,7 +13,6 @@ import {
   Create2Factory,
   Create2Factory__factory,
 } from "../../../ethers-contracts";
-import { proxyContractSalt } from "./deployments";
 
 export type ChainInfo = {
   evmNetworkId: number;
@@ -392,41 +391,23 @@ export async function getDeliveryProvider(
   return contract;
 }
 
-const deltaswapRelayerAddressesCache: Partial<Record<ChainId, string>> = {};
 export async function getDeltaswapRelayerAddress(
   chain: ChainInfo,
-  forceCalculate?: boolean,
 ): Promise<string> {
   // See if we are in dev mode (i.e. forge contracts compiled without via-ir)
   const dev = get_env_var("DEV") == "True" ? true : false;
 
-  const contracts = readContracts();
-  //If useLastRun is false, then we want to bypass the calculations and just use what the contracts file says.
-  if (!contracts.useLastRun && !lastRunOverride && !forceCalculate) {
-    const thisChainsRelayer = loadDeltaswapRelayers(dev).find(
-      (x: any) => x.chainId == chain.chainId,
-    )?.address;
-    if (thisChainsRelayer) {
-      return thisChainsRelayer;
-    } else {
-      throw Error(
-        "Failed to find a DeltaswapRelayer contract address on chain " +
-          chain.chainId,
-      );
-    }
+  const thisChainsRelayer = loadDeltaswapRelayers(dev).find(
+    (x) => x.chainId == chain.chainId,
+  )?.address;
+  if (thisChainsRelayer) {
+    return thisChainsRelayer;
+  } else {
+    throw Error(
+      "Failed to find a DeltaswapRelayer contract address on chain " +
+      chain.chainId,
+    );
   }
-
-  if (!deltaswapRelayerAddressesCache[chain.chainId]) {
-    const create2Factory = await getCreate2Factory(chain);
-    const signer = await getSigner(chain);
-      const address = await signer.getAddress();
-
-    deltaswapRelayerAddressesCache[
-      chain.chainId
-    ] = await create2Factory.computeProxyAddress(address, proxyContractSalt);
-  }
-
-  return deltaswapRelayerAddressesCache[chain.chainId]!;
 }
 
 export async function getDeltaswapRelayer(