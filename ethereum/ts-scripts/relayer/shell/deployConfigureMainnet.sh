1721bb7f7d74543927d869d413a2de5ed6573fcc
Antoniodominguess <140411318+Antoniodominguess@users.noreply.github.com>
2023-11-23 16:47:20 +0100

    update contract addresses

--- ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
@@ -1,6 +1,5 @@
-
-# This pipeline doesn't attempt to sign governance VAAs since those need to be signed by the phylaxs.
 npx tsx ./ts-scripts/relayer/config/checkNetworks.ts --set-last-run \
+  && npx tsx ./ts-scripts/relayer/create2Factory/deployCreate2Factory.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/deployDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/deltaswapRelayer/deployDeltaswapRelayer.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/configureDeliveryProvider.ts \
